01c4ad583060cc1f5408d80e52ba22d337034123
Joseph Lee <josephlee050@gmail.com>
2024-12-21 17:32:51 +0800

    Edit base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-notion-widgets/simple-countdown-timer-notion-widget/"
+  base: "/simple-countdown-timer-notion-widget/"
 })
